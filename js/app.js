88335aed628ba27630b2528dfe79cffc573726fd
ukachar <83542678+ukachar@users.noreply.github.com>
2021-12-18 10:36:35 +0100

    Dodani izracun kubika i izracun ostatka rabata
    Dodana funkcija izracun stiropora u kubike
    Dodana funkcija izracun ostatka rabata
    oboje u JS-u

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+function praznoPolje(str) {
+  return !str.trim().length;
+}
+
 const izracun_marze = () => {
   const prodajna_cijena = document.getElementById("marza_pc").value;
   const nabavna_cijena = document.getElementById("marza_nc").value;
@@ -5,12 +9,32 @@ const izracun_marze = () => {
   if (prodajna_cijena > 0 && nabavna_cijena > 0) {
     const a = prodajna_cijena - nabavna_cijena;
     const b = a * 100;
-    const ce = b / nabavna_cijena;
-    rezultat_marze.innerHTML = "Marža je " + ce;
+    const c = b / nabavna_cijena;
+    rezultat_marze.innerHTML = "Marža je " + c.toFixed(2) + " %";
   } else {
     rezultat_marze.innerHTML = "Nešto ste krivo unjeli!";
   }
-  console.log(prodajna_cijena);
-  console.log(nabavna_cijena);
-  console.log(rezultat_marze);
+};
+
+const izracun_stir_kubika = () => {
+  const kvadratura = document.getElementById("kvadr_stir").value;
+  const bunt = document.getElementById("kvadr_bunt").value;
+  const rezultat_kubika = document.getElementById("rezultat_kubika");
+  const a = kvadratura / bunt;
+  const b = a / 4;
+  return (rezultat_kubika.innerHTML = `Kubikaža je ${b} m³`);
+};
+
+const izracun_ostrbt = () => {
+  const ostrbtNc = document.getElementById("ostrbt_nc").value;
+  const ostrbtVpc = document.getElementById("ostrbt_vpc").value;
+  const rezultat_ostrbt = document.getElementById("rezultat_ostrbt");
+  const a = ostrbtNc / ostrbtVpc;
+  const b = 1 - a;
+  const c = b * 100;
+  if (praznoPolje(ostrbtVpc) || praznoPolje(ostrbtNc)) {
+    return (rezultat_ostrbt.innerHTML = "Nešto ste krivo upisali");
+  } else {
+    return (rezultat_ostrbt.innerHTML = `Ostaje ${c.toFixed(2)} % rabata.`);
+  }
 };
